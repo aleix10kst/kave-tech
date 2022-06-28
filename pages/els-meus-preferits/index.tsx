764645470761b2c8d4f26679e462e918419a083a
Aleix Canet Vidal <acanet94@gmail.com>
2022-06-29 00:27:05 +0200

    Create my favorites page.

--- pages/els-meus-preferits/index.tsx
+++ pages/els-meus-preferits/index.tsx
@@ -1,7 +1,31 @@
-import { NextPage } from "next";
+import GenericPage from "@/components/generic-page/generic-page";
+import ProductCard from "@/components/product-card/product-card";
+import { ProductsList } from "@/components/products-list/products-list";
+import { useProducts } from "@/lib/hooks/useProducts";
+import type { NextPage } from "next";
 
 export const MyFavoritesPage: NextPage = () => {
-  return <div>Preferits</div>;
+  const [fetchedProducts] = useProducts();
+
+  const products = fetchedProducts.slice();
+
+  return (
+    <GenericPage
+      title="Lista de Favoritos"
+      subtitle="Lorem ipsum dolor sit amet."
+    >
+      <ProductsList
+        products={products}
+        itemTemplate={(product, index) => (
+          <ProductCard
+            key={index}
+            product={product}
+            onClickFav={() => {}}
+          />
+        )}
+      />
+    </GenericPage>
+  );
 };
 
 export default MyFavoritesPage;
